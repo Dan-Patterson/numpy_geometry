ef907b5c0c2e6a86244d6f0b3d4788412803fb74
Dan Patterson <Dan-Patterson@users.noreply.github.com>
2025-05-08 00:43:20 -0400

    Add files via upload

--- arcpro_npg/npg/npg/_npg_samples_.py
+++ arcpro_npg/npg/npg/_npg_samples_.py
@@ -2,7 +2,7 @@
 # noqa: D205, D400, F401
 r"""
 -------------
-_load_sample_
+_npg_samples_
 -------------
 
 General description.
@@ -17,7 +17,7 @@ Author :
     <https://github.com/Dan-Patterson>`_.
 
 Modified :
-    2023-03-09
+    2025-04-28
 
 Purpose
 -------
@@ -45,8 +45,12 @@ from numpy.lib.recfunctions import structured_to_unstructured as stu  # noqa
 from numpy.lib.recfunctions import unstructured_to_structured as uts  # noqa
 
 import npg
-from npg.npg_arc_npg import fc_to_Geo  # get_shapes
-from npg.npg_plots import plot_polygons  # noqa
+from npg_io import load_geo
+
+# from npg.npg_arc_npg import fc_to_Geo    # noqa # get_shapes
+from npg.npg_plots import (plot_polygons, plot_segments,  # noqa
+                           plot_polylines, plot_mixed)  # noqa
+from npg.npg_maths import flip_left_right  # noqa
 
 # from arcpy.da import FeatureClassToNumPyArray, NumPyArrayToFeatureClass  # noqa
 # or ...
@@ -59,14 +63,15 @@ from npg.npg_plots import plot_polygons  # noqa
 #     fmt = str(a).replace("([", "\n"+" "*7)
 #     return fmt
 
-
-ft = {"bool": lambda x: repr(x.astype(np.int32)),
-      "float_kind": '{: 6.2f}'.format}  # "object":  lambda: _object_format}
 #  --- alter or use below
-
 np.set_printoptions(
     edgeitems=10, linewidth=120, precision=2, suppress=True, threshold=200,
-    formatter=ft)
+    legacy='1.25',
+    formatter={"bool": lambda x: repr(x.astype(np.int32)),
+               "float_kind": '{: 6.2f}'.format}
+    )  # legacy=False or legacy='1.25'
+
+np.ma.masked_print_option.set_display('-')  # change to a single -
 
 script = sys.argv[0]  # print this should you need to locate the script
 
@@ -78,11 +83,11 @@ script = sys.argv[0]  # print this should you need to locate the script
 
 # fc00 = r"C:\arcpro_npg\Project_npg\npgeom.gdb\Ontario_LCConic"
 # SR = "NAD 1983 Statistics Canada Lambert"
-# big = FeatureClassToNumPyArray(
+# ont = FeatureClassToNumPyArray(
 #     fc00, ['OID@', 'SHAPE@X', 'SHAPE@Y'], spatial_reference=SR,
 #     explode_to_points=True)
-# minX = np.min(big["SHAPE@X"])
-# minY = np.min(big["SHAPE@Y"])
+# minX = np.min(ont["SHAPE@X"])
+# minY = np.min(ont["SHAPE@Y"])
 # Ont = fc_to_Geo(
 #     fc00, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info="")
 
@@ -90,51 +95,67 @@ minX = 300000
 minY = 5000000
 SR = r'NAD 1983 CSRS MTM  9'
 
-# -- square
+# -- squares
 aoi = np.array([[0., 0.], [0., 10.], [10., 10.], [10., 0.], [0., 0.]])
+aoi0 = np.array([[0., 4.], [4., 10.], [10., 6.], [6., 0.], [0., 4.]])
 
-# -- sq2
-# fc0 = r'C:\\arcpro_npg\\Project_npg\\tests.gdb\\sq2'
-# sq2 = fc_to_Geo(fc0, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info='')
-# sq2 = npg.roll_coords(sq2)
-# b0, b1, b2, b3, b4, b5, b6 = sq2.outer_rings(False)
 
-# -- pl, cl
-# fc1 = r"C:\arcpro_npg\Project_npg\TestShapes.gdb\clp"
-# fc2 = r"C:\arcpro_npg\Project_npg\TestShapes.gdb\poly"
-# cl_ = fc_to_Geo(fc1, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info="")
-# pl_ = fc_to_Geo(fc2, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info="")
-# cl_ = npg.roll_coords(cl_)
-# pl_ = npg.roll_coords(pl_)
+# ---- sq2 npz
+sq2_pth = r"C:\arcpro_npg\data\npz_npy\sq2.npz"
+sq2 = load_geo(sq2_pth, extras=False, prn_info=False)
+sq2 = npg.roll_coords(sq2)
+b0, b1, b2, b3, b4, b5, b6 = sq2.outer_rings(False)
+
+# ---- pl, cl npy
+pth = r"C:\arcpro_npg\data\npz_npy\pl_cl_data.npy"
+pl_cl_data = np.load(pth)
+oid_ = pl_cl_data['OBJECTID']
+shp = pl_cl_data['Shape']
+shp = shp - [300000., 5000000.]
+dif = np.nonzero(oid_[1:] - oid_[:-1] != 0)[0] + 1
+a_ = np.array_split(shp, dif)
+pl_cl = npg.arrays_to_Geo(a_, kind=2)  # pl_cl split into its components
+
+# pl_cl_data = npg.npg_io.load_geo(pth, extras=False, prn_info=False)
+
+# ---- multi geo
+pth = r"C:\arcpro_npg\data\npz_npy\multi.npz"
+multi = npg.npg_io.load_geo(pth, extras=False, prn_info=False)
+multi, arrs, names = npg.npg_io.load_geo(pth, True, True)
+
+# -- edgy1, eclip npz
+edgy1 = npg.npg_io.load_geo(r"C:\arcpro_npg\data\npz_npy\edgy1.npz")
+eclip = npg.npg_io.load_geo(r"C:\arcpro_npg\data\npz_npy\eclip.npz")
 # #
-# fc3 = r"C:\arcpro_npg\Project_npg\npgeom.gdb\edgy1"
-# fc4 = r"C:\arcpro_npg\Project_npg\npgeom.gdb\e_clip"
-# edgy1 = fc_to_Geo(fc3, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info="")
-# edgy1 = npg.roll_coords(edgy1)
-# eclip = fc_to_Geo(fc4, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info="")
-# eclip = npg.roll_coords(eclip)
-
-#
 # fc5 = r"C:\arcpro_npg\Project_npg\TestShapes.gdb\d0_"
 # d0_ = fc_to_Geo(fc5, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info="")
 # d0_ = npg.roll_coords(d0_)
 # d0_ = d0_.XY  # ** d0 is good **
 
+# ---- letter arrays
 laps = np.array([[0., 0.], [0., 6.], [0., 2.], [0., 10.], [0., 11.],
                  [0., 10.], [6., 10.], [2., 10.], [10., 10.],
-                 [10., 0.], [0., 0.]])
+                 [10., 0.], [0., 0.]])  # overlapping points
 
 A = np.array([[1., 0.], [4, 10.], [6., 10.], [9., 0.], [7., 0.], [6., 4.],
               [4., 4.], [3., 0.], [1., 0.]])
 
-B = np.array([[1., 1.], [3., 9.], [7., 9.], [8., 8.], [8., 6.], [5., 5.],
+B = np.array([[1., 1.], [2., 8.], [3., 9.], [7., 9.], [8., 8.], [8., 6.],
+              [5., 5.],
               [8., 4.], [8., 2.], [7., 1.], [1., 1.]])
 
 C = np.array([[0., 0.], [0., 10.], [10., 10.], [10., 8.], [2., 8.], [2., 2.],
               [10., 2.], [10., 0.], [0., 0.]])
 
-d0_ = np.array([[0., 5.], [3., 7.], [5., 10.], [7., 7.], [10., 5.], [7., 3.],
-                [5., 0.], [3., 3.], [0., 5.]])
+D = np.array([
+    np.array([[1., 1.], [2., 9.], [7., 9.], [8., 8.], [8., 6.],
+              [8., 4.], [7.5, 2.], [6.5, 1.], [1., 1]]),
+    np.array([[3., 8.], [2., 2.], [6., 2.], [7., 4.], [7., 6.], [6.5, 8.],
+              [3., 8.]])], dtype='O')
+
+D0 = np.array([[0., 5.], [5., 10.], [10., 5.], [5., 0.], [0., 5.]])
+
+# D1 = npg.roll_coords(D0)
 
 E = np.array([[0., 0.], [0., 10.], [10., 10.], [10., 8.], [2., 8.],
               [2., 5.5], [5., 5.5], [5., 4], [2., 4.], [2., 2.],
@@ -143,24 +164,222 @@ E = np.array([[0., 0.], [0., 10.], [10., 10.], [10., 8.], [2., 8.],
 F = np.array([[0., 0.], [1., 10.], [10., 10.], [9., 8.], [3., 8.], [2., 5.5],
               [5., 5.5], [5., 4.], [3., 4.], [2., 0.], [0., 0.]])
 
+G = np.array([[0., 0.], [1., 10.], [10., 10.], [9.5, 8.], [3., 8.],
+              [2.5, 2.], [7.5, 2.], [8., 4.], [5., 4.], [5.5, 5.5],
+              [10., 5.5], [9., 0.], [0., 0.]])
+
+H = np.array([[0., 0.], [2., 10.], [4., 10.], [3., 6.], [7., 6.],
+              [8., 10.], [10., 10.], [8., 0], [6., 0.], [7., 4.],
+              [3., 4.], [2., 0.], [0., 0.]])
+
 Io = np.array([[0., 0.], [2., 10.], [4., 10.], [2., 0.], [0., 0.]])
 
 K = np.array([[0., 0.], [2., 10.], [4., 10.], [3., 5], [6., 10], [8., 10.],
               [5., 4.], [6., 0.], [4., 0.], [3., 3.], [2., 0.], [0., 0.]])
 
+M = np.array([[0., 0.], [1., 10.], [4., 10.], [5., 9.], [6., 10], [9., 10.],
+              [10., 0.], [8., 0.], [7., 7.], [5., 5.], [3., 7.], [2., 0.],
+              [0., 0.]])
 
-# ont_attr = r"C:\arcpro_npg\data\ontario_attrib.npz"
-# ont_geo = r"C:\arcpro_npg\data\ontario_sp.npz"
-# #  C:\arcpro_npg\Project_npg\npgeom.gdb
-# ont_bnds = r"C:\arcpro_npg\data\Ont_bnds.npz"
-# # geo, arrs, names0 = npg.npg_io.load_geo(ont_geo, extras=True, prn_info=False)
+W = np.array([[1.0, 0.0], [0.0, 10.0], [2.0, 10.0], [3., 3.], [5., 5.],
+              [7., 3.], [8., 10.], [10.0, 10.0], [9.0, 0.0],
+              [6., 0.], [5., 1.], [4., 0.], [1.0, 0.0]])  # np.flip(E, 1)
+
+
+d0_ = np.array([[0., 5.], [3., 7.], [5., 10.], [7., 7.], [10., 5.], [7., 3.],
+                [5., 0.], [3., 3.], [0., 5.]])
+
+# p00, c00 arrays
+p00 = np.array([[0., 0], [0., 2], [8., 2], [8., 0.], [0., 0]])
+p01 = np.array([[0., 0], [0., 2], [8., 2], [1, 1.], [8., 0.], [0., 0]])
+p02 = np.array([[0., 0], [7, 1.], [0., 2], [8., 2], [8., 0.], [0., 0]])
+p03 = np.array([[0., 0], [0., 2], [2., 2], [4, 4.], [6., 2], [8., 2],
+                [8., 0.], [0., 0]])
+c00 = np.array([[1., 1.], [2., 2.], [5., 2.], [6., 1.], [1., 1.]])
+c01 = np.array([[1., 1.], [3., 3.], [4., 3.], [6., 1.], [1., 1.]])
+c02 = np.array([[1., 1.], [3., 3.], [3.5, 1.5], [4., 3.], [6., 1.],
+                [1., 1.]])
+c03 = np.array([[1., 1.], [2., 2], [3., 3.], [3.5, 1.5], [4., 3.],
+                [6., 1.], [1., 1.]])  # extra point on line
+c04 = np.array([[1., 1.], [3., 3.], [6., 1.], [1., 1.]])  # triangle
+
+# ---- a geo array from D0
+
+di = npg.arrays_to_Geo([D0], 2, "D0", False)
+did = npg.densify_by_factor(di, factor=3)
+did = npg.arrays_to_Geo(did, kind=2, info="", to_origin=False)
+# for plotting
+# lbls = np.arange(0, len(did.XY))
+# plot_polygons(did.bits, outline=True, vertices=True, labels=lbls)
+# lexsort examples
+#    self = did
+#  np.lexsort((self.Y, self.X))
+#  array([ 0, 12, 13, 11,  1, 10,  2,  9,  3,  8,  4,  7,  5,  6])
+#
+# ---- list
+# try npg.flatten(lst) to unpack
+lst = [
+       [[1, 2, 3], [4, 5]],
+       [[1, 2]],
+       [1, 2],
+       [[]],
+       [[1, 2, 3], [4, 5], [6, 7, 8]],
+       [[[1, 2, 3], [4, 5]], [[6, 7, 8]]],
+       ]
+
+d1_ = np.array([[0., 5.], [0, 6], [2, 6], [3, 5], [2, 8],
+                [4, 9], [5., 10.], [5, 5], [7.5, 7.5], [10., 5.],
+                [4, 4], [5., 0.], [4, 1], [2, 2], [0., 5.]])
+
+
+d0_ = np.array([[0., 5.], [3., 7.], [5., 10.], [7., 7.], [10., 5.], [7., 3.],
+                [5., 0.], [3., 3.], [0., 5.]])
+# ---- new example
+
+poly0 = np.array([[6., 1.], [1.2, 1.], [4.5, 6.0], [5.3, 5.5],
+                  [3.0, 1.9], [6.0, 1.9], [6.0, 1.0]])
+poly1 = np.array([[5.5, 2.9], [3.0, 1.2], [4.5, 3.1], [2.2, 5.5],
+                  [3.0, 6.0], [5.5, 2.9]])
+
+# maple leaf https://math-drills.com/geometry/coordinate_point_art_001.php
+#  created below and saved as a geo array
+# ---- maple leaf
+ml = npg.npg_io.load_geo("C:/arcpro_npg/data/npz_npy/maple_leaf.npz")
+
+# quarter ml
+"""
+to_add = np.array([[11., 4.], [5., 4], [0., 8.]])
+sub = np.concatenate((ml[6:16], to_add), axis=0)
+poly = np.copy(sub)
+
+ml_rb = np.array([[8., 9], [16.0, 9.0], [18.0, 8.0], [13.0, 4.0], [14.0, 3.0],
+                  [10.0, 4.0], [11.0, 1.0], [11.0, 0.0], [9.0, 4.0], [8., 4.],
+                  [8.,9.]])
+
+poly = np.copy(ml_rb)
+
+#test output
+z1 = _offset_np_(poly, buff_dist=1.0, offset_check=True, as_segments=False)
+z1_2 = _offset_np_(poly, buff_dist=1.5, offset_check=True, as_segments=False)
+z2 = _offset_np_(poly, buff_dist=2, offset_check=True, as_segments=False)
+z3 = _offset_np_(poly, buff_dist=3.0, offset_check=True, as_segments=False)
+z4 = _offset_np_(poly, buff_dist=4.0, offset_check=True, as_segments=False)
+
+plot_polygons([z1, z1_2, z2, z3, z4])
+
+# checking for crossing segments after offset is done using `_cross_chk_`
+# 4 rotated shapes
+
+cr0 = np.array([[0., 0], [2, 1], [0, 2], [2, 3],
+                [0, 4], [3, 4], [4, 0], [0, 0]], dtype='float')
+
+cr1 = np.array([[0., 0], [2, 1], [0, 1.5], [2, 2], [0, 2],
+                [2, 3], [0, 4], [3, 4], [4, 0], [0, 0]], dtype='float')
+
+poly = np.copy(cr1)  # could replace with cr0 for less severe angles
+
+tmp = npg.npg_geom_hlp._trans_rot_(poly, 90, True)
+cr2 = npg.roll_arrays([tmp])
+
+tmp = npg.npg_geom_hlp._trans_rot_(poly, 180, True)
+cr3 = npg.roll_arrays([tmp])
+
+tmp = npg.npg_geom_hlp._trans_rot_(poly, 270, True)
+cr4 = npg.roll_arrays([tmp])
+
+for polygon overlay stuff
+
+ply_a = np.copy(cr1)
+ply_b = flip_left_right(cr1, shift_back=True)
+
+"""
+# counterclockwise
+# ML = np.array([[1, -3], [5, -4], [4, -3], [9, 1], [7, 2], [8, 5], [5, 4],
+#                [5, 5], [3, 4], [4, 9], [2, 7], [0, 10], [-2, 7], [-4, 8],
+#                [-3, 3], [-5, 6], [-5, 4], [-8, 5], [-7, 2], [-9, 1],
+#                [-4, -3], [-5, -4], [0, -3], [2, -7], [2, -6], [1, -3]],
+#               dtype='float')
+# # use this for clockwise
+# ml = np.array([[1, -3], [2, -6], [2, -7], [0, -3], [-5, -4], [-4, -3],
+#                [-9, 1], [-7, 2], [-8, 5], [-5, 4], [-5, 6], [-3, 3], [-4, 8],
+#                [-2, 7], [0, 10], [2, 7], [4, 9], [3, 4], [5, 5], [5, 4],
+#                [8, 5], [7, 2], [9, 1], [4, -3], [5, -4], [1, -3]],
+#               dtype='float')
+
+# polygon example from https://github.com/shapely/shapely/issues/564
+#
+# shifted to minimum
+p_arc = np.array([
+    [0.00, 0.00], [1.27, 1.27], [1.35, 1.35], [1.35, 0.91], [1.35, 0.88],
+    [1.07, 0.79], [0.95, 0.72], [0.94, 0.72], [0.83, 0.63],
+    [0.73, 0.53], [0.72, 0.52], [0.64, 0.41], [0.63, 0.41], [0.56, 0.29],
+    [0.56, 0.28], [0.51, 0.15], [0.50, 0.14], [0.47, 0.01], [0.47, 0.00],
+    [0.03, 0.00], [0.00, 0.00]])  # removed  [0.83, 0.63], a duplicate
+
+# test corners for buffer `arc`
+ps_c_pe = [[[-1., 10], [0., 10.], [0., 11.]],
+           [[10., 11], [10., 10.], [11, 10.]],
+           [[11, 0.], [10., 0], [10., -1]],
+           [[0., -1], [0, 0.], [-1., 0]]]
+a_cor = np.array(ps_c_pe)
+
+# this one works with offset_buffer
+#
+# off_square = np.array([[-2., 2.], [2, 2], [2, -2], [-2, -2], [-2, 2]])
+# off2 = offset_buffer(off_square, 0.5)
+# plot_polygons([off_square, off2])
+#
+# ---- geojson example
+
+pth = r"C:\arcpro_npg\data\json\poly_a.geojson"
+poly_a = npg.npg_io.geojson_to_geo(pth, kind=2, info=None, to_origin=True)
+pth1 = r"C:\arcpro_npg\data\json\p.geojson"
+pj = npg.npg_io.load_geojson(pth1, full=False, just_geometry=True)
+min_extent = poly_a.XT.min(axis=0)
+pnts_ = pj - min_extent
+# -- to plot pj and p use...
+# data = [[poly_a.bits, 2, 'red', '.', True], [pnts_, 0, 'black', 'o', False]]
+# plot_mixed(data, title="", invert_y=False, ax_lbls=None)
+#
+
+# ---- Ontario data
+"""
+ont_attr = "C:/arcpro_npg/data/npz_npy/ontario_attrib.npz"
+ont_geo = "C:/arcpro_npg/data/npz_npy/ontario_sp.npz"
+#  C:/arcpro_npg/Project_npg/npgeom.gdb
+ont_bnds = "C:/arcpro_npg/data/npz_npy/Ont_bnds.npz"
+geo, arrs, names0 = npg.npg_io.load_geo(ont_geo, extras=True, prn_info=False)
+# names0  # ['g', 'ift', 'kind', 'extents', 'spatial_ref']
+
 # bg = npg.npg_io.load_geo(ont_geo, extras=False, prn_info=False)
 # bg2 = npg.npg_io.load_geo(ont_bnds, extras=False, prn_info=False)
 
-# names, arrs = npg.npg_io.load_geo_attr(ont_attr, prn_info=False)
-# n0, n1 = names
-# bg_attr = arrs[n0]
-# flds = arrs[n1]
+names, arrs = npg.npg_io.load_geo_attr(ont_attr, prn_info=False)
+n0, n1 = names[0], names[1]
+bg_attr = arrs[n0]
+flds = arrs[n1]
+"""
+
+"""
+# requires arcpy
+fc0 = r'C:\\arcpro_npg\\Project_npg\\tests.gdb\\sq2'
+sq2 = fc_to_Geo(fc0, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info='')
+
+-- pl, cl
+fc1 = "C:/arcpro_npg/Project_npg/TestShapes.gdb/clp"
+fc2 = "C:/arcpro_npg/Project_npg/TestShapes.gdb/poly"
+cl_ = fc_to_Geo(fc1, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info="")
+pl_ = fc_to_Geo(fc2, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info="")
+cl_ = npg.roll_coords(cl_)
+pl_ = npg.roll_coords(pl_)
+#
+fc3 = "C:/arcpro_npg/Project_npg/npgeom.gdb/edgy1"
+fc4 = "C:/arcpro_npg/Project_npg/npgeom.gdb/e_clip"
+edgy1 = fc_to_Geo(fc3, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info="")
+edgy1 = npg.roll_coords(edgy1)
+eclip = fc_to_Geo(fc4, geom_kind=2, minX=minX, minY=minY, sp_ref=SR, info="")
+eclip = npg.roll_coords(eclip)
+"""
 
 # -- reported error for clip
 # p1 = [(38.0, 60.6), (37.9, 60.4), (33.7, 56.9), (33.2, 56.5), (32.6, 56.4),
@@ -190,18 +409,19 @@ if __name__ == "__main__":
     - `bg_attr` : attribute array for the above
     - `multi` : multipart shape with holes
     - `d0_`    : star-like
-    - `A`, `B`, `C`, `E`, `F`, `Io`, `K` : letters
+    - `A`, `B`, `C`, `D0`, `E`, `F`, `G`, `H`, `Io`, `K`, `M`, `W` : letters
     - `edgy1` : two large singlepart polygons
     - `eclip  : to clip `edgy`
+    - `pl_cl` : pl_cl geoarray
     - `sq`    : four connected shapes, first with a triangular hole.
     - `sq2`   : like `sq` but with 7 shapes, some multipart, some with holes.
     - b0, b1, c0, c1 parts of sq2 used for testing
-    - edgy1, eclip - clip testing
+    - `ml`    : the maple leaf
     """
     print(dedent(msg).format(script))
 
     # -- load geo
-    # pth = r"c:\arcpro_npg\data\ontario_sp.npz"
+    # pth = r"c:\arcpro_npg\data\npz_npy\ontario_sp.npz"
     # g, arrs, names  = npg.npg_io.load_geo(pth, suppress_extras=False)
     # ---- my shapes
 
@@ -215,11 +435,11 @@ if __name__ == "__main__":
     #
     # -- save geo
     # f_name =  r"multi.npz"
-    # folder = r"c:\arcpro_npg\data"
+    # folder = r"c:\arcpro_npg\data\npz_npy"
     # npg.npg_io.save_geo(multi, f_name, folder)
     #
     # -- load geo
-    # pth = r"c:\arcpro_npg\data\multi.npz"
+    # pth = r"C:\arcpro_npg\data\npz_npy\multi.npz"
     # multi = npg.npg_io.load_geo(pth, extras=False, prn_info=False)
     # multi, arrs, names  = npg.npg_io.load_geo(pth, True, True)
     #
@@ -238,10 +458,10 @@ if __name__ == "__main__":
     # fc4 = r"C:\arcpro_npg\Project_npg\npgeom.gdb\Polylines"
     # fc5 = r"C:\arcpro_npg\Project_npg\npgeom.gdb\dissolved"
     # #
-    # j0 = r"C:\arcpro_npg\Project_npg\Data_files\multi.geojson"
-    # j1 = r"C:\arcpro_npg\Project_npg\Data_files\multi.json"
-    # j2 = r"C:\arcpro_npg\Project_npg\Data_files\sq2.geojson"
-    # j3 = r"C:\arcpro_npg\Project_npg\Data_files\sq.json"
+    j0 = r"C:\arcpro_npg\data\json\multi.geojson"
+    # j1 = r"C:\arcpro_npg\data\json\multi.json"
+    # j2 = r"C:\arcpro_npg\data\json\sq2.geojson"
+    # j3 = r"C:\arcpro_npg\data\json\sq.json"
     # c = np.array([[0, 10.], [11., 13.], [12., 7.], [8., 2], [0., 10]])
     #
     # fc7 = r"C:\arcpro_npg\Project_npg\tests.gdb\hex_shift"
@@ -315,19 +535,6 @@ if __name__ == "__main__":
     # dt = np.dtype([('Xs', 'f8'), ('Ys', 'f8')])
     # z0 = z.view(dt).squeeze()
 
-    p00 = np.array([[0., 0], [0., 2], [8., 2], [8., 0.], [0., 0]])
-    p01 = np.array([[0., 0], [0., 2], [8., 2], [1, 1.], [8., 0.], [0., 0]])
-    p02 = np.array([[0., 0], [7, 1.], [0., 2], [8., 2], [8., 0.], [0., 0]])
-    p03 = np.array([[0., 0], [0., 2], [2., 2], [4, 4.], [6., 2], [8., 2],
-                    [8., 0.], [0., 0]])
-    c00 = np.array([[1., 1.], [2., 2.], [5., 2.], [6., 1.], [1., 1.]])
-    c01 = np.array([[1., 1.], [3., 3.], [4., 3.], [6., 1.], [1., 1.]])
-    c02 = np.array([[1., 1.], [3., 3.], [3.5, 1.5], [4., 3.], [6., 1.],
-                    [1., 1.]])
-    c03 = np.array([[1., 1.], [2., 2], [3., 3.], [3.5, 1.5], [4., 3.],
-                    [6., 1.], [1., 1.]])  # extra point on line
-    c04 = np.array([[1., 1.], [3., 3.], [6., 1.], [1., 1.]])  # triangle
-
     # # line crosses both outside
     # s00 = np.array([[1., 1], [1., 9.], [9., 9.], [9., 1], [1., 1.]])
     # s01 = np.array([[1., 1], [1., 2], [1., 8.], [1., 9.], [2., 9.],
@@ -398,6 +605,8 @@ if __name__ == "__main__":
 
     # -- lines
 
+    # crossed = np.array([[0., 0.], [5., 5.], [0., 5.], [5., 0.]])
+    #
     # z0 = np.array([[0., 0], [8., 8.], [2., 2.], [10., 10]])
     # z1 = np.array([[10., 0], [2., 8.], [8., 2.], [0., 10]])
     # z2 = np.array([[2., 0], [2, 7.], [2., 3.], [2., 10]])
